ea747aeef480ca455f7a13919c50a69ea9dab422
Debora C. Fernandes <dehcarrascoza@gmail.com>
2021-02-25 12:04:44 -0300

    incluindo o contexto countdownprovider

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,11 +1,13 @@
+import Head from 'next/head';
+
 import { CompletedChallenges } from '../components/CompletedChallenges'
 import { Countdown } from '../components/Countdown'
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
 import { ChallengeBox } from '../components/ChallengeBox'
-import styles from '../styles/pages/Home.module.css'
 
-import Head from 'next/head';
+import styles from '../styles/pages/Home.module.css'
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 export default function Home() {
   return (
@@ -16,18 +18,19 @@ export default function Home() {
 
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
 
-        <div>
-          <ChallengeBox />
-        </div>
-
-      </section>
     </div>
   )
 }
